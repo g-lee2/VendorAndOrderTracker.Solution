d634b769b938db8d11caf675e221ce76e8c6a693
Grace Lee <gracexsnoopy@gmail.com>
2022-12-10 12:51:54 -0800

    Pass seventh test for vendor class

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -25,6 +25,10 @@ namespace VendorAndOrderTracker.Models
     {
       _instances.Clear();
     }
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
   }
 }
 
