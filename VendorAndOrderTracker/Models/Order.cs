21df101ae49bea8950334f1be04a5913161b04b7
Grace Lee <gracexsnoopy@gmail.com>
2022-12-10 04:16:51 -0800

    Pass first test after creating constructors for order and vendor class

--- VendorAndOrderTracker/Models/Order.cs
+++ VendorAndOrderTracker/Models/Order.cs
@@ -7,7 +7,14 @@ namespace VendorAndOrderTracker.Models
     public string Title { get; set; }
     public string Description { get; set; }
     public double Price { get; set; }
-    public int Date { get; set; }
+    public double Date { get; set; }
     public int Id { get; }
+    public Order(string title, string description, double price, double date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+    }
   }
 }
--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -4,8 +4,16 @@ namespace VendorAndOrderTracker.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor> {};
     public string VendorName { get; set; }
     public string Description { get; set; }
     public int Id { get; }
+
+    public Vendor(string vendorName, string description)
+    {
+      VendorName = vendorName;
+      Description = description;
+      _instances.Add(this);
+    }
   }
 }
