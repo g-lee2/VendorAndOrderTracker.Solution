2513ebe5dbad851eb14d667efc63050528f82235
Grace Lee <gracexsnoopy@gmail.com>
2022-12-10 13:31:37 -0800

    Pass eleventh test for order class

--- VendorAndOrderTracker/Models/Order.cs
+++ VendorAndOrderTracker/Models/Order.cs
@@ -31,5 +31,10 @@ namespace VendorAndOrderTracker.Models
     {
       return _instances;
     }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
   }
 }
