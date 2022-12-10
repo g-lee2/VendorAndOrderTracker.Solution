b02856843d2e653b469802f3709a4c0b2e809f3f
Grace Lee <gracexsnoopy@gmail.com>
2022-12-10 04:26:52 -0800

    Write second test for vendor class

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,16 @@ namespace VendorAndOrderTracker.Tests
       Vendor newVendor = new Vendor("vendor", "sells pastry");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetVendorName_ReturnsVendorName_String()
+    {
+      string vendorName = "London Pastry";
+
+      Vendor newVendor = new Vendor(vendorName, "sells pastry");
+      string result = newVendor.VendorName;
+
+      Assert.AreEqual(vendorName, result);
+    }
   }
 }
