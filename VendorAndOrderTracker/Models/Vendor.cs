9d4390ea8d2e0dbdea0ebee75dcf2bf4aa4711c6
Grace Lee <gracexsnoopy@gmail.com>
2022-12-10 04:58:44 -0800

    Change list details

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -4,7 +4,7 @@ namespace VendorAndOrderTracker.Models
 {
   public class Vendor
   {
-    private static List<Vendor> _instances = new List<Vendor> {};
+    private static List<Order> _instances = new List<Order> {};
     public string VendorName { get; set; }
     public string Description { get; set; }
     public int Id { get; }
