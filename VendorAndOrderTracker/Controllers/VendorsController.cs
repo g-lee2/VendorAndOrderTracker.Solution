d964d170f959b2aada8178d82ab5fdd9c1128af8
Grace Lee <gracexsnoopy@gmail.com>
2022-12-10 13:12:54 -0800

    Update routes so users can click an individual vendor and see details

--- VendorAndOrderTracker/Controllers/VendorsController.cs
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -4,26 +4,33 @@ using System.Collections.Generic;
 
 namespace VendorAndOrderTracker.Controllers
 {
-  public class VendorController : Controller
+  public class VendorsController : Controller
   {
-    [HttpGet("/vendor")]
+    [HttpGet("/vendors")]
     public ActionResult Index()
     {
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
 
-    [HttpGet("/vendor/new")]
-    public ActionResult CreateForm()
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
     {
       return View();
     }
 
-    [HttpPost("/vendor")]
+    [HttpPost("/vendors")]
     public ActionResult Create(string vendorName, string description)
     {
       Vendor aVendor = new Vendor(vendorName, description);
       return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Vendor foundVendor = Vendor.Find(id);
+      return View(foundVendor);
     }
   }
 }
