ea57f2ee7b7591fc6b9ca56f7ff86a9fc5d2e15c
Grace Lee <gracexsnoopy@gmail.com>
2022-12-10 13:37:05 -0800

    Remove GetAll method and write/fail test for vendor class first

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -16,15 +16,12 @@ namespace VendorAndOrderTracker.Models
       _instances.Add(this);
       Id = _instances.Count;
     }
-
-    public static List<Vendor> GetAll()
-    {
-      return _instances;
-    }
+    
     public static void ClearAll()
     {
       _instances.Clear();
     }
+
     public static Vendor Find(int searchId)
     {
       return _instances[searchId-1];
